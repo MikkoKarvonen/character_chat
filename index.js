9e023d4139ae689eb6ee5cb9aa45d43e28930787
Mikko <mkarvonen34@gmail.com>
2020-06-14 11:20:56 +0300

    Add start script

--- index.js
+++ index.js
@@ -37,6 +37,8 @@ io.on("connection", (socket) => {
   });
 });
 
-http.listen(3000, () => {
-  console.log("listening on *:3000");
+const port = process.env.PORT || 3000;
+
+http.listen(port, () => {
+  console.log(`Listening on localhost:${port}`);
 });
